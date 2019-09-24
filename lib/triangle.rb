7c2ed7b7a8c7433408ce424d7f712c8897630d60
Joseph Sesti <sestijoey63@gmail.com>
2019-09-24 16:06:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,9 @@ class Triangle
   end
   
   def kind
-    if length == width && width == height
+    if length == width && width == height && height == length
       return :equilateral
+    elsif width < length && width < height
+      return :isosceles
     elsif 
 end
